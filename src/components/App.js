a615faf31e9467146e3e9b321dff0569b11f297b
Ben Goodrow <bpgoodrow@gmail.com>
2022-09-20 09:27:29 -0700

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./SignIn";
 
 function App(){
   return (
-    <React.Fragment>
+    <Router>
       <Header />
-      <TicketControl />
-    </React.Fragment>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<TicketControl />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfjn8R9A1PsOKziO014Sq-iyjKtCyiGdg",
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 
-export default db;
+export { db, auth };
